908c62f5ad473e5e6d3f4661cfadb78f37b3a7b0
Mike Seese <seesemichaelj@gmail.com>
2023-10-15 22:14:26 -0700

    initial implementation to kill direct descendant processes

---
+++ src/utils/kill.ts
@@ -0,0 +1,23 @@
+import {  exec } from 'child_process';
+
+export function killProcessTree(pid: number, signal: string = "SIGKILL", callback: (error: Error | null) => void = () => {}) {
+    if (Number.isNaN(pid)) {
+        // if (callback) {
+        //     return callback(new Error("pid must be a number"));
+        // } else {
+            throw new Error("pid must be a number");
+        // }
+    }
+
+    switch (process.platform) {
+        case "win32": {
+            exec(`taskkill /pid ${pid} /T /F`, callback);
+            break;
+        }
+        default: {
+            exec(`pkill -${signal} -P ${pid}`, callback); // kill direct child processes; no need to kill entire process tree
+            exec(`kill -${signal} -P ${pid}`, callback); // kill parent process
+            break;
+        }
+    }
+}
